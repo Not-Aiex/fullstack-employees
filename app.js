69bbffd5e7dd2ff55e7030b133c1b3fcfccffd6d
Alex Chen <achen2291@gmail.com>
2026-07-22 18:29:42 -0700

    working version

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 import express from "express";
 const app = express();
 export default app;
+import employeesRouter from "#api/employees";
 
-// TODO: this file!
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Fullstack Employees API.");
+});
+
+app.use("/employees", employeesRouter);
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Something went wrong");
+});
